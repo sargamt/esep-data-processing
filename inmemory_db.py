31c7a2781ec544e107f350c794a76a71f53e5a64
Sargam Thakur <sargamthakur665@gmail.com>
2025-12-05 19:04:55 -0500

    get, put, commit

--- inmemory_db.py
+++ inmemory_db.py
@@ -1,21 +1,42 @@
-# inmemory db
-
 class InMemoryDB:
 
     def __init__(self):
         self.accounts = {}
+        self.inTransaction = False
+        self.current_transaction = {}
     
     def get(self, key: str) -> int:
-        pass
+        if key in self.accounts:
+            return self.accounts[key]
+        else:
+            return None
 
     def put(self, key: str, val: int):
-        pass
+        if not self.inTransaction:
+            raise Exception("A transaction is not in progress.")
+        else:
+            self.current_transaction[key] = val
 
     def begin_transaction(self):
         pass
 
     def commit(self):
+        if self.current_transaction:
+            for key, val in self.current_transaction.items():
+                self.accounts[key] = val
+            self.current_transaction = {}
         pass
 
     def rollback(self):
         pass
+
+
+def main():
+    imdb = InMemoryDB
+
+    imdb.get("A")
+
+    pass
+
+
+main()
